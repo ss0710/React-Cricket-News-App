da15b9a9cddd70c35ea1cd39774bfae216a6fb7a
Sudheer Singh <sudheersid0710@gmail.com>
2020-10-23 20:39:51 +0530

    Updates

---
+++ precache-manifest.3bd21b7c5dbaa2d8b616a2cd655394e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8706cf054ed00ff00c055b5d9653eba4",
+    "url": "/React-Cricket-News-App/index.html"
+  },
+  {
+    "revision": "a90c9fc44b981a8e4edc",
+    "url": "/React-Cricket-News-App/static/css/main.fc150aac.chunk.css"
+  },
+  {
+    "revision": "7abca6b80e0d5720b2da",
+    "url": "/React-Cricket-News-App/static/js/2.39d43e80.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/React-Cricket-News-App/static/js/2.39d43e80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a90c9fc44b981a8e4edc",
+    "url": "/React-Cricket-News-App/static/js/main.3e12ee6a.chunk.js"
+  },
+  {
+    "revision": "c61fb28783712700513a",
+    "url": "/React-Cricket-News-App/static/js/runtime-main.de2763ef.js"
+  },
+  {
+    "revision": "912ffd1560b64f9dd931bffe105f862e",
+    "url": "/React-Cricket-News-App/static/media/crickback.912ffd15.jpg"
+  }
+]);
